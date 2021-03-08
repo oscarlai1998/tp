60a0da4e9241434482df3a9e14bd89fb8b4c9869
Fuxi <fuxi_xia@u.nus.edu>
2021-03-08 17:05:10 +0800

    Add getCommand() to parser

--- src/main/java/seedu/duke/Parser.java
+++ src/main/java/seedu/duke/Parser.java
@@ -1,4 +1,10 @@
 package seedu.duke;
 
+import java.util.Scanner;
+
 public class Parser {
+    public String getCommand() {
+        Scanner in = new Scanner(System.in);
+        return in.nextLine();
+    }
 }
