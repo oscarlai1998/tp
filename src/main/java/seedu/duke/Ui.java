f35a14131aa8da8e752975d752357ae953b0da75
kewenlok <e0360650@u.nus.edu>
2021-03-08 14:43:08 +0800

    Implement exit message

--- src/main/java/seedu/duke/Ui.java
+++ src/main/java/seedu/duke/Ui.java
@@ -4,4 +4,8 @@ public class Ui {
     public void printsWelcomeMessage() {
         System.out.println("Welcome to iGraduate!");
     }
+
+    public void printsExitMessage() {
+        System.out.println("Goodbye. See you again!");
+    }
 }
