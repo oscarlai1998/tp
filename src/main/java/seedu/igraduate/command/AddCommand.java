271c82051396b396a6967a9c92d9dfd3ad3bd38d
kewenlok <e0360650@u.nus.edu>
2021-03-09 08:50:22 +0800

    Implement skeleton code for command classes

--- src/main/java/seedu/igraduate/command/AddCommand.java
+++ src/main/java/seedu/igraduate/command/AddCommand.java
@@ -1,4 +1,32 @@
 package seedu.igraduate.command;
 
+import seedu.igraduate.Storage;
+import seedu.igraduate.ModuleList;
+import seedu.igraduate.Ui;
+
+/**
+ * Handles add command.
+ */
 public class AddCommand extends Command {
+    /**
+     * Todo: Add comments here
+     *
+     * @param moduleList Module list consisting of all modules.
+     * @param ui User interface for printing result.
+     * @param storage Storage for storing module list data.
+     */
+    @Override
+    public void execute(ModuleList moduleList, Ui ui, Storage storage) {
+        // Todo: Command action
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return false
+     */
+    @Override
+    public boolean isExit() {
+        return false;
+    }
 }
