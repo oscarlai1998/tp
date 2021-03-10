461c716e592314f65ad711fda8fa70dd3f7aa194
kewenlok <e0360650@u.nus.edu>
2021-03-10 20:04:34 +0800

    Implement getter and setter methods

--- src/main/java/seedu/igraduate/module/Module.java
+++ src/main/java/seedu/igraduate/module/Module.java
@@ -12,11 +12,67 @@ public class Module {
 
     public Module(String code, String name, double credit, String status, String grade,
                   ArrayList<String> preRequisites) {
+        setCode(code);
+        setName(name);
+        setCredit(credit);
+        setStatus(status);
+        setGrade(grade);
+        setPreRequisites(preRequisites);
+    }
+
+    public void setCode(String code) {
         this.code = code;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public void setCredit(double credit) {
         this.credit = credit;
+    }
+
+    public void setStatus(String status) {
         this.status = status;
+    }
+
+    public void setGrade(String grade) {
         this.grade = grade;
+    }
+
+    public void setPreRequisites(ArrayList<String> preRequisites) {
         this.preRequisites = preRequisites;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCredit() {
+        return credit;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getStatusIcon() {
+        String status = getStatus();
+        if (status.equals("taken")) {
+            return "✓";
+        } else if (status.equals("not taken")) {
+            return "✘";
+        } else {
+            return "-";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %-8s %50s %8d", getStatusIcon(), getCode(), getName(), getCredit());
     }
 }
