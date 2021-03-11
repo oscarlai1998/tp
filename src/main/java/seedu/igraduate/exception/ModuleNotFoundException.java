08b6ebf97cf289e083f78e4806bfef5781111cba
kewenlok <e0360650@u.nus.edu>
2021-03-11 22:35:10 +0800

    Add module not found exception class

---
+++ src/main/java/seedu/igraduate/exception/ModuleNotFoundException.java
@@ -0,0 +1,10 @@
+package seedu.igraduate.exception;
+
+public class ModuleNotFoundException extends Exception {
+    private static final String MODULE_NOT_FOUND_ERROR_MESSAGE = "The module code you have entered"
+            + " does not exists. \nPlease double check and try again.";
+
+    public ModuleNotFoundException () {
+        super(MODULE_NOT_FOUND_ERROR_MESSAGE);
+    }
+}
