e8ade3d0541fc42332fba8ccba56a433e3827104
xseh <sehxinru@gmail.com>
2021-03-11 19:34:26 +0800

    Add JUnit tests

--- src/test/java/seedu/igraduate/IGraduateTest.java
+++ src/test/java/seedu/igraduate/IGraduateTest.java
@@ -1,8 +1,11 @@
 package seedu.igraduate;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+
+import seedu.igraduate.exception.InvalidCommandException;
 
 class IGraduateTest {
 }
---
+++ src/test/java/seedu/igraduate/ParserTest.java
@@ -0,0 +1,18 @@
+package seedu.igraduate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.igraduate.exception.InvalidCommandException;
+
+public class ParserTest {
+    @Test
+    void parseCommand_emptyCommand_exceptionThrown() {
+        String line = "";
+        Exception exception = assertThrows(InvalidCommandException.class, () -> Parser.parseCommand(line));
+        assertEquals("The command you have entered"
+        + " is incorrect. \nPlease double check and try again.", exception.getMessage());
+    }
+}
