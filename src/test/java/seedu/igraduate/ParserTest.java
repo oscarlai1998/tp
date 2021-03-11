792a673476a7436d5cc42e49577342511fa1bcb6
Fuxi <fuxi_xia@u.nus.edu>
2021-03-11 21:12:21 +0800

    Add JUnit test to check exception for wrong parameters

--- src/test/java/seedu/igraduate/ParserTest.java
+++ src/test/java/seedu/igraduate/ParserTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.igraduate.exception.IncorrectParameterCountException;
 import seedu.igraduate.exception.InvalidCommandException;
 
 public class ParserTest {
@@ -14,5 +15,16 @@ public class ParserTest {
         Exception exception = assertThrows(InvalidCommandException.class, () -> Parser.parseCommand(line));
         assertEquals("The command you have entered is incorrect. \nPlease double check and try again.",
                 exception.getMessage());
+    }
+
+    @Test
+    void executeAddCommand_tooManyParameters_exceptionThrown() {
+        String line = "Add CS2113 -t core -c 4 hoi";
+        String[] components = line.split(" ");
+        Exception exception = assertThrows(IncorrectParameterCountException.class,
+                () -> Parser.executeAddCommand(components));
+        assertEquals("The number of parameters"
+                + " provided is incorrect. \nPlease double check and try again.", exception.getMessage());
+
     }
 }
