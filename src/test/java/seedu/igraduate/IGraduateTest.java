3952d222285451d74fecc4e4da7464fbd0f55e67
xseh <sehxinru@gmail.com>
2021-03-11 19:42:15 +0800

    Fix gradle issues

--- src/test/java/seedu/igraduate/IGraduateTest.java
+++ src/test/java/seedu/igraduate/IGraduateTest.java
@@ -12,7 +12,7 @@ class IGraduateTest {
     void parseCommand_emptyCommand_exceptionThrown() {
         String line = "";
         Exception exception = assertThrows(InvalidCommandException.class, () -> Parser.parseCommand(line));
-        assertEquals("The command you have entered"
-        + " is incorrect. \nPlease double check and try again.", exception.getMessage());
+        assertEquals("The command you have entered" + " is incorrect. \nPlease double check and try again.",
+                exception.getMessage());
     }
 }
