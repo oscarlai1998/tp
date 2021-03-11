505385b482628afcb617beb91ec81701de45798a
kewenlok <e0360650@u.nus.edu>
2021-03-11 22:38:54 +0800

    Modify code to comply with coding standards

--- src/main/java/seedu/igraduate/exception/ModuleNotFoundException.java
+++ src/main/java/seedu/igraduate/exception/ModuleNotFoundException.java
@@ -4,7 +4,7 @@ public class ModuleNotFoundException extends Exception {
     private static final String MODULE_NOT_FOUND_ERROR_MESSAGE = "The module code you have entered"
             + " does not exists. \nPlease double check and try again.";
 
-    public ModuleNotFoundException () {
+    public ModuleNotFoundException() {
         super(MODULE_NOT_FOUND_ERROR_MESSAGE);
     }
 }
