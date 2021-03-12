02d758f98afc2446faf9c5d0c7685ba1cca152f3
kewenlok <e0360650@u.nus.edu>
2021-03-12 09:54:55 +0800

    Rename exception class

--- src/main/java/seedu/igraduate/exception/LoadModuleFailException.java
+++ src/main/java/seedu/igraduate/exception/LoadModuleFailException.java
@@ -1,14 +1,14 @@
 package seedu.igraduate.exception;
 
 /**
- * The exception is thrown if module cannot be imported properly.
+ * The exception is thrown if module cannot be loaded properly.
  */
-public class ImportModuleFailException extends Exception {
-    private static final String IMPORT_MODULE_FAIL_ERROR_MESSAGE = "Oops! An error occur when"
+public class LoadModuleFailException extends Exception {
+    private static final String LOAD_MODULE_FAIL_ERROR_MESSAGE = "Oops! An error occur when"
             + " trying to import module data from file :(.";
 
-    public ImportModuleFailException() {
-        super(IMPORT_MODULE_FAIL_ERROR_MESSAGE);
+    public LoadModuleFailException() {
+        super(LOAD_MODULE_FAIL_ERROR_MESSAGE);
     }
 }
 
