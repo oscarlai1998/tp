2274080f278780fd55ffa16a73428720ce2d2387
kewenlok <e0360650@u.nus.edu>
2021-03-13 17:26:34 +0800

    Add support to handle input not number exception

---
+++ src/main/java/seedu/igraduate/exception/InputNotNumberException.java
@@ -0,0 +1,10 @@
+package seedu.igraduate.exception;
+
+public class InputNotNumberException extends Exception {
+    private static final String INPUT_NOT_NUMBER_ERROR_MESSAGE = "The input for the following"
+            + " parameter provided is not a number: \n";
+
+    public InputNotNumberException(String parameterType) {
+        super(INPUT_NOT_NUMBER_ERROR_MESSAGE + parameterType);
+    }
+}
