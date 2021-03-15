a34b9865a099ce8f25851d492dc0b31f94004b0f
Oscar Lai <e0424620@u.nus.edu>
2021-03-15 16:54:52 +0800

    Assertion and Logs

--- src/main/java/seedu/igraduate/command/ListCommand.java
+++ src/main/java/seedu/igraduate/command/ListCommand.java
@@ -4,10 +4,15 @@ import seedu.igraduate.Storage;
 import seedu.igraduate.ModuleList;
 import seedu.igraduate.Ui;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 /**
  * Handles list command.
  */
 public class ListCommand extends Command {
+    private static Logger logger = Logger.getLogger(ListCommand.class.getName());
 
     /**
      * Print list of all modules.
@@ -19,8 +24,11 @@ public class ListCommand extends Command {
     @Override
     public void execute(ModuleList moduleList, Ui ui, Storage storage) {
         if (moduleList.isEmpty()) {
+            assert moduleList.isEmpty() : "List should be empty";
+            logger.log(Level.INFO, "List is empty.");
             ui.printListEmptyMessage();
         } else {
+            logger.log(Level.INFO, "List is not empty.");
             ui.printEntireList(moduleList.getModules());
         }
     }
