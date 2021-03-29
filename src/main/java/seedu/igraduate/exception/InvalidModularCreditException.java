abf456c73b0103d04a1c80f92d8509c2e1f75b65
Oscar Lai <e0424620@u.nus.edu>
2021-03-29 20:38:25 +0800

    Gradle error

--- src/main/java/seedu/igraduate/exception/InvalidModularCreditException.java
+++ src/main/java/seedu/igraduate/exception/InvalidModularCreditException.java
@@ -4,8 +4,8 @@ package seedu.igraduate.exception;
  * Exception is thrown if modular credit input is negative.
  */
 public class InvalidModularCreditException extends Exception {
-    public static final String INVALID_MODULAR_CREDIT_ERROR_MESSAGE = "Invalid modular credit input. " +
-        "Please enter a positive number.";
+    public static final String INVALID_MODULAR_CREDIT_ERROR_MESSAGE = "Invalid modular credit input. "
+        + "Please enter a positive number.";
 
     public InvalidModularCreditException() {
         super(INVALID_MODULAR_CREDIT_ERROR_MESSAGE);
