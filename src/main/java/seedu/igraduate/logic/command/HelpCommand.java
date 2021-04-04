08d9e473a89b0b792fdba1c92353d856b94c52d6
kewenlok <e0360650@u.nus.edu>
2021-04-05 00:43:50 +0800

    Add logging message

--- src/main/java/seedu/igraduate/logic/command/HelpCommand.java
+++ src/main/java/seedu/igraduate/logic/command/HelpCommand.java
@@ -5,7 +5,11 @@ import seedu.igraduate.model.list.ModuleList;
 import seedu.igraduate.storage.Storage;
 import seedu.igraduate.ui.Ui;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class HelpCommand extends Command {
+    private static final Logger LOGGER = Logger.getLogger(ListCommand.class.getName());
     private String helpType;
 
     public HelpCommand(String helpType) {
@@ -22,6 +26,7 @@ public class HelpCommand extends Command {
      */
     @Override
     public void execute(ModuleList moduleList, Ui ui, Storage storage) throws InvalidCommandException {
+        LOGGER.log(Level.INFO, "Executing help command...");
         switch (helpType.toLowerCase()) {
         case "no params":
             ui.printIntroHelp();
@@ -51,8 +56,10 @@ public class HelpCommand extends Command {
             ui.printExitHelp();
             break;
         default:
+            LOGGER.log(Level.WARNING, "Invalid help option...");
             throw new InvalidCommandException();
         }
+        LOGGER.log(Level.INFO, "End of help command execution.");
     }
 
     @Override
